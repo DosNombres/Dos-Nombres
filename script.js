8192de9b10dc1cc1a20b1d541206dfea42265fb5
DosNombres <dosnombrescafe@gmail.com>
2026-06-21 16:16:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-function abrirProducto(nombre, imagen, descripcion){
 
-    document.getElementById("modal").style.display = "block";
-
-    document.getElementById("modalTitulo").innerText = nombre;
-
-    document.getElementById("modalImagen").src = imagen;
-
-    document.getElementById("modalDescripcion").innerText = descripcion;
-}
-
-function cerrarProducto(){
-
-    document.getElementById("modal").style.display = "none";
-
-}
